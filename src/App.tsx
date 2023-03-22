a0b88bf98271e7acf4bfaf941897a340d20f7767
szymonkwak <szymon.kwak@gmail.com>
2023-03-22 11:54:35 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Footer, Header, MainContent, Sidebar } from './components';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Sidebar />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Header } from './Header/Header';
+export { default as MainContent } from './MainContent/MainContent';
+export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as Footer } from './Footer/Footer';
